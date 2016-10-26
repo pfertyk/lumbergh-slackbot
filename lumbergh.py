7cf782d25cb68686c6af6be5b0ddbb13f4e6ab9f
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-26 20:59:45 +0200

    Use flask instead of infinite loop

--- lumbergh.py
+++ lumbergh.py
@@ -1,31 +1,32 @@
 from slackclient import SlackClient
-import time
 import os
+from flask import Flask, request, Response
 
-token = os.environ.get('SLACKBOT_LUMBERGH_TOKEN')
 
-sc = SlackClient(token)
-
+app = Flask(__name__)
 link = '<https://cdn.meme.am/instances/400x/33568413.jpg|That would be great>'
 
-if sc.rtm_connect():
-    while True:
-        events = sc.rtm_read()
-        for event in events:
-            if (
-                'channel' in event and
-                'text' in event and
-                event.get('type') == 'message'
-            ):
-                channel = event['channel']
-                text = event['text']
-                if 'that would be great' in text.lower() and link not in text:
-                    sc.api_call(
-                        'chat.postMessage',
-                        channel=channel,
-                        text=link,
-                        as_user='true:'
-                    )
-        time.sleep(1)
-else:
-    print('Connection failed, invalid token?')
+client_token = os.environ.get('SLACKBOT_LUMBERGH_TOKEN')
+webhook_token = os.environ.get('SLACKBOT_WEBHOOK_TOKEN')
+
+sc = SlackClient(client_token)
+
+
+@app.route('/lumbergh', methods=['POST'])
+def inbound():
+    print('incoming')
+    if request.form.get('token') == webhook_token:
+        print('verified')
+        channel = request.form.get('channel_name')
+        text = request.form.get('channel_name')
+        if 'that would be great' in text.lower() and link not in text:
+            sc.api_call(
+                'chat.postMessage',
+                channel=channel,
+                text=link,
+                as_user='true:'
+            )
+    return Response(), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
